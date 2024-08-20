5ca1cfe0888ea534d6a9676414c4a9edeab260b1
aruma256 <22253623+aruma256@users.noreply.github.com>
2024-08-20 23:59:47 +0900

    RotationHelperの1.21対応

--- src/client/java/com/github/lotqwerty/lottweaks/client/RotationHelper.java
+++ src/client/java/com/github/lotqwerty/lottweaks/client/RotationHelper.java
@@ -308,7 +308,7 @@ public class RotationHelper {
 				}
 				List<Item> items = new ArrayList<>();
 				for (String itemStr: line.split(",")) {
-					ResourceLocation resourceLocation = new ResourceLocation(itemStr);
+					ResourceLocation resourceLocation = ResourceLocation.parse(itemStr);
 					Item item = BuiltInRegistries.ITEM.get(resourceLocation);
 					if (item == null || item == Items.AIR) {
 						warnGroupConfigErrors(String.format("'%s' is not supported.", itemStr), lineCount, group);
